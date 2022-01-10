0ab17a654fe55beb89c2574e4af35abe3bb5b736
youseonHwang <dbtjs0221@gmail.com>
2022-01-10 14:25:46 +0900

    web-site 서버 및 eureka 서버 생성

---
+++ msa-web-site/src/main/java/com/example/demo/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
+}
